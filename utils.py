fa4713ac22e8ee47547579f7c7a6e7503171b12e
HoneyBadger919 <antonio.paro96@gmail.com>
2022-04-09 01:11:02 +0200

    ml preprocessing part

--- utils.py
+++ utils.py
@@ -2,7 +2,9 @@ import requests
 import time
 import os
 import sys
+import tensorflow as tf
 
+#function to make REST requests with status checking
 def try_request(link, iter = 10):
     
     while iter > 0:
@@ -12,3 +14,12 @@ def try_request(link, iter = 10):
         time.sleep(1)
         iter -= 1
     sys.exit('Server not responding correctly')
+
+#function to create windowed datasets
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+  dataset = tf.data.Dataset.from_tensor_slices(series)
+  dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+  dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+  dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1][1]))
+  dataset = dataset.batch(batch_size).prefetch(1)
+  return dataset
